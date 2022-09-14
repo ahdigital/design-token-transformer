ff095898e4931b998b0efdb6abfe1ed6bdf6034f
ahdigital <ahdigital@users.noreply.github.com>
2022-09-14 10:51:42 +0000

    Tokens updated

--- build/ios-swift/StyleDictionaryColor.swift
+++ build/ios-swift/StyleDictionaryColor.swift
@@ -4,18 +4,14 @@
 //
 
 // Do not edit directly
-// Generated on Fri, 15 Jul 2022 19:49:49 GMT
+// Generated on Wed, 14 Sep 2022 10:51:41 GMT
 
 
 import UIKit
 
 public enum StyleDictionaryColor {
-    public static let colorsEmpty = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0)
-    public static let colorsMultipleFills0 = UIColor(red: 0.251, green: 1.000, blue: 0.729, alpha: 1)
-    public static let colorsMultipleFills1 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0.10196078431372549)
-    public static let colorsRefBlue = UIColor(red: 0.016, green: 0.290, blue: 1.000, alpha: 1)
-    public static let colorsSemiRed = UIColor(red: 1.000, green: 0.373, blue: 0.016, alpha: 0.5019607843137255)
-    public static let colorsSingleBlue = UIColor(red: 0.016, green: 0.290, blue: 1.000, alpha: 1)
-    public static let colorsSpecialCharacters = UIColor(red: 0.251, green: 0.875, blue: 0.314, alpha: 1)
-    public static let colorsSpecialCharactersNderung = UIColor(red: 0.204, green: 0.337, blue: 0.686, alpha: 1)
+    public static let colorBrandBlue = UIColor(red: 0.173, green: 0.580, blue: 0.745, alpha: 1)
+    public static let colorBrandGreen = UIColor(red: 0.231, green: 0.620, blue: 0.529, alpha: 1)
+    public static let colorBrandOchre = UIColor(red: 0.878, green: 0.682, blue: 0.133, alpha: 1)
+    public static let colorBrandOrange = UIColor(red: 0.961, green: 0.353, blue: 0.000, alpha: 1)
 }
--- build/ios/StyleDictionaryColor.m
+++ build/ios/StyleDictionaryColor.m
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Fri, 15 Jul 2022 19:49:49 GMT
+// Generated on Wed, 14 Sep 2022 10:51:41 GMT
 
 
 #import "StyleDictionaryColor.h"
@@ -21,14 +21,10 @@
 
   dispatch_once(&onceToken, ^{
     colorArray = @[
-[UIColor colorWithRed:1.000f green:0.373f blue:0.016f alpha:0.502f],
-[UIColor colorWithRed:0.251f green:1.000f blue:0.729f alpha:1.000f],
-[UIColor colorWithRed:0.000f green:0.000f blue:0.000f alpha:0.102f],
-[UIColor colorWithRed:0.016f green:0.290f blue:1.000f alpha:1.000f],
-[UIColor colorWithRed:0.016f green:0.290f blue:1.000f alpha:1.000f],
-[UIColor colorWithRed:0.000f green:0.000f blue:0.000f alpha:0.000f],
-[UIColor colorWithRed:0.251f green:0.875f blue:0.314f alpha:1.000f],
-[UIColor colorWithRed:0.204f green:0.337f blue:0.686f alpha:1.000f]
+[UIColor colorWithRed:0.961f green:0.353f blue:0.000f alpha:1.000f],
+[UIColor colorWithRed:0.173f green:0.580f blue:0.745f alpha:1.000f],
+[UIColor colorWithRed:0.231f green:0.620f blue:0.529f alpha:1.000f],
+[UIColor colorWithRed:0.878f green:0.682f blue:0.133f alpha:1.000f]
     ];
   });
 
